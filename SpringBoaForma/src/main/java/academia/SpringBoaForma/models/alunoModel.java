88e4cc6912b8a4551c9b1ad97d3f931090ff6e99
ivens magno <ivensmagno@gmail.com>
2025-01-26 14:06:29 -0300

    finalizacao do alunoModel a primeira parte

--- SpringBoaForma/src/main/java/academia/SpringBoaForma/models/alunoModel.java
+++ SpringBoaForma/src/main/java/academia/SpringBoaForma/models/alunoModel.java
@@ -1,8 +1,12 @@
 package academia.SpringBoaForma.models;
 
 import jakarta.persistence.*;
-
 import java.io.Serializable;
+import java.util.Date;
+
+//private String servicoEscolhido;
+//private String tipoPagamento;
+//private String qualObjetivo;
 
 @Entity
 @Table(name = "tb_alunos")
@@ -12,4 +16,148 @@ public class alunoModel implements Serializable {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
     private Long idAluno;
+    public String nomeAluno;
+    private Date dataNascimento;
+    private Date dataRegistroSistema;
+    private String numeroTelefonicoAluno;
+    private String numeroEnergenciaAluno;
+    private String rua;
+    private String cpf;
+    public Boolean doenca;
+    private String descricaoDoenca;
+
+    public Boolean getAcessoAlunoSistema() {
+        return acessoAlunoSistema;
+    }
+
+    public void setAcessoAlunoSistema(Boolean acessoAlunoSistema) {
+        this.acessoAlunoSistema = acessoAlunoSistema;
+    }
+
+    public Float getValorPago() {
+        return valorPago;
+    }
+
+    public void setValorPago(Float valorPago) {
+        this.valorPago = valorPago;
+    }
+
+    public Date getValidadeDaMensalidade() {
+        return validadeDaMensalidade;
+    }
+
+    public void setValidadeDaMensalidade(Date validadeDaMensalidade) {
+        this.validadeDaMensalidade = validadeDaMensalidade;
+    }
+
+    public Date getValidadeMensalidadePeloDiaPago() {
+        return ValidadeMensalidadePeloDiaPago;
+    }
+
+    public void setValidadeMensalidadePeloDiaPago(Date validadeMensalidadePeloDiaPago) {
+        ValidadeMensalidadePeloDiaPago = validadeMensalidadePeloDiaPago;
+    }
+
+    public Date getDataPagamento() {
+        return dataPagamento;
+    }
+
+    public void setDataPagamento(Date dataPagamento) {
+        this.dataPagamento = dataPagamento;
+    }
+
+    public String getSenhaAluno() {
+        return senhaAluno;
+    }
+
+    public void setSenhaAluno(String senhaAluno) {
+        this.senhaAluno = senhaAluno;
+    }
+
+    public String getDescricaoDoenca() {
+        return descricaoDoenca;
+    }
+
+    public void setDescricaoDoenca(String descricaoDoenca) {
+        this.descricaoDoenca = descricaoDoenca;
+    }
+
+    public Boolean getDoenca() {
+        return doenca;
+    }
+
+    public void setDoenca(Boolean doenca) {
+        this.doenca = doenca;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getNumeroEnergenciaAluno() {
+        return numeroEnergenciaAluno;
+    }
+
+    public void setNumeroEnergenciaAluno(String numeroEnergenciaAluno) {
+        this.numeroEnergenciaAluno = numeroEnergenciaAluno;
+    }
+
+    public String getNumeroTelefonicoAluno() {
+        return numeroTelefonicoAluno;
+    }
+
+    public void setNumeroTelefonicoAluno(String numeroTelefonicoAluno) {
+        this.numeroTelefonicoAluno = numeroTelefonicoAluno;
+    }
+
+    public Date getDataRegistroSistema() {
+        return dataRegistroSistema;
+    }
+
+    public void setDataRegistroSistema(Date dataRegistroSistema) {
+        this.dataRegistroSistema = dataRegistroSistema;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getNomeAluno() {
+        return nomeAluno;
+    }
+
+    public void setNomeAluno(String nomeAluno) {
+        this.nomeAluno = nomeAluno;
+    }
+
+    public Long getIdAluno() {
+        return idAluno;
+    }
+
+    public void setIdAluno(Long idAluno) {
+        this.idAluno = idAluno;
+    }
+
+    private String senhaAluno;
+    public Date dataPagamento;
+    public Date ValidadeMensalidadePeloDiaPago;
+    private Date validadeDaMensalidade;
+    public Float valorPago;
+    public Boolean acessoAlunoSistema;
 }
